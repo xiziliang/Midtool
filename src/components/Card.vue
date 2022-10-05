8b352964c5b7b92e2479c3f709d06d068046a785
席子良 <934887857@qq.com>
2022-10-05 20:48:01 +0800

    feat(animate): add animate

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
-import { computed } from "vue";
-import { useVModel, useVModels } from "@vueuse/core";
+import { computed, ref } from "vue";
+import { useVModel } from "@vueuse/core";
+import { Check } from "@element-plus/icons-vue";
+
 interface List {
   // Level1Label: string;
   // KeyWord: string;
@@ -18,23 +20,66 @@ const props = defineProps<{
 
 const emit = defineEmits(["update:data"]);
 
-const list = useVModel(props, "data", emit, {
+const propsList = useVModel(props, "data", emit, {
   passive: true,
   deep: true,
 });
 
+// 已选list
+const selectedList = ref(propsList.value.filter((x) => x.isSelected));
+// 未选list
+const remainList = ref(propsList.value.filter((x) => !x.isSelected));
+
 function onTrigger(item: List) {
+  if (item.isSelected) {
+    selectedList.value = selectedList.value.filter((x) => x.promptEN !== item.promptEN);
+    remainList.value.unshift(item);
+  } else {
+    selectedList.value.push(item);
+    remainList.value = remainList.value.filter((x) => x.promptEN !== item.promptEN);
+  }
   item.isSelected = !item.isSelected;
 }
 </script>
 <template>
+  <TransitionGroup name="animate__fadeInRight">
+    <div
+      class="card"
+      mt-4
+      v-for="item in selectedList"
+      :key="item.promptEN"
+      @click="onTrigger(item)"
+      :class="{ selected: item.isSelected }"
+    >
+      <el-button
+        v-if="item.isSelected"
+        class="check-icon"
+        type="success"
+        :icon="Check"
+        circle
+      />
+      <div class="card-img">
+        <img height="160" width="156" :src="item.imgName" />
+      </div>
+      <div class="card-name" p-2>{{ item.promptZH }}</div>
+      <div class="card-enname" p-2>{{ item.promptEN }}</div>
+    </div>
+  </TransitionGroup>
   <div
     class="card"
-    mt-2
-    v-for="item in list"
+    mt-4
+    v-for="item in remainList"
+    :key="item.promptEN"
     @click="onTrigger(item)"
     :class="{ selected: item.isSelected }"
   >
+    <el-button
+      v-if="item.isSelected"
+      class="check-icon"
+      type="success"
+      :icon="Check"
+      circle
+    />
     <div class="card-img">
       <img height="160" width="156" :src="item.imgName" />
     </div>
@@ -70,5 +115,11 @@ function onTrigger(item: List) {
   img {
     width: 100%;
   }
+}
+
+.check-icon {
+  position: absolute;
+  top: -16px;
+  right: -16px;
 }
 </style>
