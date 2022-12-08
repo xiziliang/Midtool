692ce72512c19720fe834021cf2a89e9cac48fe9
席子良 <934887857@qq.com>
2022-12-08 18:08:42 +0800

    feat: add NOTE

--- src/utils/getImage.ts
+++ src/utils/getImage.ts
@@ -7,5 +7,7 @@ export function getImageUrl (url: string){
   ], { eager: true });
   const imageUrl = (Object.entries(images).find(x => x[0].includes(url))?.[1] as any)?.default;
 
+  // NOTE: imageUrl是相对路径
+  // NOTE: 通过相对路径我们就能得到一个被完整解析的静态资源 URL.
   return new URL(imageUrl, import.meta.url).href;
 };
